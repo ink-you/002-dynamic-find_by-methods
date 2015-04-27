e3867cf4993a30928e0bcad3e5c6496e66f3e375
kangkyu <kangkyu1111@gmail.com>
2015-04-27 15:14:03 -0700

    use dynamic find_by

--- app_name/app/controllers/tasks_controller.rb
+++ app_name/app/controllers/tasks_controller.rb
@@ -1,10 +1,12 @@
 class TasksController < ApplicationController
 
   def incomplete
-    @tasks = Task.find(:all, :conditions => ['complete = ?', false])
+    # @tasks = Task.find(:all, :conditions => ['complete = ?', false])
+    @tasks = Task.find_all_by_complete(false)
   end
 
   def last_incomplete
-    @tasks = Task.find(:all, :conditions => ['complete = ?', false], :order => 'created_at DESC')
+    # @tasks = Task.find(:all, :conditions => ['complete = ?', false], :order => 'created_at DESC')
+    @tasks = Task.find_all_by_complete(false, :order => 'created_at DESC')
   end
 end
